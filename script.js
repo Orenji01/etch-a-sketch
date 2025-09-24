961153f9e63d69a8c872e5cdc16ba1043ee570f0
LACRIM94 <anasmestour@gmail.com>
2025-09-24 12:17:47 +0200

    Make a  hover effect that turns the squares red

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const container = document.querySelector("#container");
 
 for (let i = 0; i < 256; i++) {
-    const divSquare = document.createElement("div")
+    let divSquare = document.createElement("div")
     container.appendChild(divSquare);
 }
+
+const element = document.querySelector("div");
+element.classList.add("change-color")
+
+const changeColor = document.querySelector(".change-color")
+changeColor.addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = "red";
+    container.style.backgroundColor = "white";
+});
