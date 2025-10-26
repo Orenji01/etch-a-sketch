4fcebd4e83f25320403a48a868854d656bd6149d
LACRIM94 <anasmestour@gmail.com>
2025-10-26 13:33:28 +0100

    Add a function that changes grid size
    Make a button that when clicked asks the user how big they want their
    grid size to be and when they have inputted their desired size the grid
    changes

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ changeColor.addEventListener("mouseover", (event) => {
     event.target.style.backgroundColor = "red";
     container.style.backgroundColor = "white";
 });
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    let gridSize = prompt("Type how big you want your grid size to be");
+    if (isNaN(gridSize) === false && gridSize < 100) {
+        changeColor.parentNode.removeChild(changeColor);
+        const newContainer = document.querySelector("#new-container")
+        for (let i = 0; i < gridSize * gridSize; i++) {
+            let divSquare = document.createElement("div")
+            newContainer.appendChild(divSquare);
+        }
+    }
+    console.log(gridSize)
+})
