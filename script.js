28ad239989969ced26de327e3a9828e9d9034f78
LACRIM94 <anasmestour@gmail.com>
2025-10-28 15:08:07 +0100

    Adress Errors that make the new grid not work

--- script.js
+++ script.js
@@ -17,13 +17,25 @@ changeColor.addEventListener("mouseover", (event) => {
 const button = document.querySelector("button");
 button.addEventListener("click", () => {
     let gridSize = prompt("Type how big you want your grid size to be");
-    if (isNaN(gridSize) === false && gridSize < 100) {
+    if (isNaN(gridSize) === false && Number(gridSize) < 101) {
+
         changeColor.parentNode.removeChild(changeColor);
         const newContainer = document.querySelector("#new-container")
         for (let i = 0; i < gridSize * gridSize; i++) {
             let divSquare = document.createElement("div")
             newContainer.appendChild(divSquare);
         }
+        newContainer.style.width = 10 * Number(gridSize) + "px";
+        newContainer.style.height = 10 * Number(gridSize) + "px";
+        newContainer.style.border = "1px solid";
+
+        const newSquares = document.querySelector("div");
+        newSquares.classList.add("new-change-color")
+
+        const newChangeColor = document.querySelector(".new-change-color")
+        newChangeColor.addEventListener("mouseover", (event) => {
+            event.target.style.backgroundColor = "red";
+            newContainer.style.backgroundColor = "white";
+        });
     }
-    console.log(gridSize)
 })
